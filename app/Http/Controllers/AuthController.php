10e58a6b5b9aa86428654c5986db205a3d8d7789
FlapJac-k <eslamce122@gmail.com>
2025-07-13 00:23:39 +0300

    Added UserResource

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,12 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\UserResource;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
+    public function me(Request $request)
+    {
+        return new UserResource($request->user());
+    }
+
     public function login(Request $request)
     {
         $request->validate(['email' => 'required|email', 'password' => 'required']);
--- routes/api.php
+++ routes/api.php
@@ -2,16 +2,13 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\TaskController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
 
-    Route::get('/user', function (Request $request) {
-        return $request->user();
-    });
+    Route::get('/user', [AuthController::class, 'me']);
 
     Route::post('logout', [AuthController::class, 'logout']);
 
